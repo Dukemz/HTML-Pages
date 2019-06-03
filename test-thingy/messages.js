c389fd2c3f14ea382582f49e8b2586dceb2fbf0f
Dukemz <46872741+Dukemz@users.noreply.github.com>
2019-06-03 15:40:43 +0100

    Create messages.js

---
+++ test-thingy/messages.js
@@ -0,0 +1,8 @@
+console.log("%cHello!", "font-size: 27px; color:orange; text-shadow: -3px -3px #e8ba3f, 5px 5px yellow");
+console.log("I mean, I don't blame you, but why did you HAVE to open inspect?");
+console.log("Just don't tell RubRub I'm here. He can't know.");
+// console.log("Do you have any questions?");
+// console.log("[Questions you can ask: Whos-RubRub,]");
+
+// console.log("What do you mean who's RubRub?");
+// console.log("Uhh, nevermind.")

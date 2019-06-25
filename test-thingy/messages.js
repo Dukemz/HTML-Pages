22131101d7ebe619f49a857d629cb2cd45382c3a
Dukemz <46872741+Dukemz@users.noreply.github.com>
2019-06-25 13:14:00 +0100

    Update messages.js

--- test-thingy/messages.js
+++ test-thingy/messages.js
@@ -1,5 +1,5 @@
-console.log("%cHello!", "font-size: 27px; color:orange; text-shadow: -3px -3px #e8ba3f, 5px 5px yellow");
-console.log("I mean, I don't blame you, but why did you HAVE to open inspect?");
+console.log("%cWhy are you here?", "font-size: 27px; color:orange; text-shadow: -3px -3px #e8ba3f, 5px 5px yellow");
+console.log("I mean, I don't blame you for being curious, but why did you HAVE to open inspect?");
 console.log("Just don't tell RubRub I'm here. He can't know.");
 // console.log("Do you have any questions?");
 // console.log("[Questions you can ask: Whos-RubRub,]");
